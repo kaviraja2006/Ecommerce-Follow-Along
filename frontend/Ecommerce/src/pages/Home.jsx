be07eb1a70d0c5161b1c06d7601167ebffb7407a
kavi <mkvr2006@gmail.com>
2025-01-31 17:14:01 +0530

    done

---
+++ frontend/Ecommerce/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-6xl font-semibold">Welcome to Home</h1>
+    </div>
+  );
+}
+
+export default Home;
